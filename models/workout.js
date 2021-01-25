b6e95a4a74f206400ed4cb18dd603c695c19f822
Steve Tong <stevetong53@yahoo.com>
2021-01-24 23:25:49 -0800

    changes to mongo to connect with heroku

--- models/workout.js
+++ models/workout.js
@@ -1,54 +1,61 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const workoutSchema = new Schema({
-  day: {
-    type: Date,
-    default: Date.now,
+const workoutSchema = new Schema(
+  {
+    day: {
+      type: Date,
+      default: Date.now,
+    },
+    exercises: [
+      {
+        name: {
+          type: String,
+          required: "Enter an exercise name",
+          trim: true,
+        },
+      },
+      {
+        type: {
+          type: String,
+          required: "Enter an exercise type",
+          trim: true,
+        },
+      },
+      {
+        duration: {
+          type: Number,
+          required: "Enter an exercise duration",
+        },
+      },
+      {
+        weight: {
+          type: Number,
+        },
+      },
+      {
+        sets: {
+          type: Number,
+        },
+      },
+      {
+        reps: {
+          type: Number,
+        },
+      },
+      {
+        distance: {
+          type: Number,
+        },
+      },
+    ],
   },
-  exercises: [
-    {
-      name: {
-        type: String,
-        required: "Enter an exercise name",
-        trim: true,
-      },
+  {
+    toJSON: {
+      virtuals: true,
     },
-    {
-      type: {
-        type: String,
-        required: "Enter an exercise type",
-        trim: true,
-      },
-    },
-    {
-      duration: {
-        type: Number,
-        required: "Enter an exercise duration",
-      },
-    },
-    {
-      weight: {
-        type: Number,
-      },
-    },
-    {
-      sets: {
-        type: Number,
-      },
-    },
-    {
-      reps: {
-        type: Number,
-      },
-    },
-    {
-      distance: {
-        type: Number,
-      },
-    },
-  ],
-});
+  }
+);
 
 workoutSchema.virtual("totalDuration").get(function () {
   return this.exercises.reduce((total, exercise) => {
